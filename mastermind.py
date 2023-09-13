c644c119bfe4a895993308a4a0e9685e4a2780c5
PMC <144255533+700xt@users.noreply.github.com>
2023-09-13 12:02:07 +0200

    implementation chaine de caractere

--- mastermind.py
+++ mastermind.py
@@ -1,4 +1,5 @@
 from random import choice
+from sys import exit
 
 #Generation de chaine de caractere
 first = choice(['R','G','B','Y','P','W'])
@@ -7,27 +8,79 @@ third = choice(['R','G','B','Y','P','W'])
 four = choice(['R','G','B','Y','P','W'])
 solve = (first+second+third+four)
 print (solve)
-
-
-
+b=12
+fi_caract = str()
+se_caract = str()
+th_caract = str()
+fo_caract = str()
+nb_tentative = 12
 
 
 #Boucle tentative
 a=0
-while a<12:
-    print(f"Mastermind => Rappel des couleurs disponible :(R)ed, (G)reen, (B)lue, (Y)ellow, (P)urple et (W)hite")
-    combination = input ("Combinaison couleur :")
+while a<nb_tentative: 
+    print(f"Mastermind => (R)ed, (G)reen, (B)lue, (Y)ellow, (P)urple et (W)hite")
+    input("La solution sera sous forme YRZZ pour Y: mauvaise endroit")
+    combination = input ("Combinaison couleur sous forme rywp:")
     combination = combination.upper ()
-    b=0
-    for i in combination:
-        print ("i vaut", i)
+    c=0
     if combination == solve :
         print("Victoire")
+        print(f"score : {score} ")
+        exit()
+    for letter in combination:
+        if c==0:
+            if letter==first:
+                fi_caract=first
+            elif letter==second:
+                fi_caract="X"
+            elif letter==third:
+                fi_caract="X"
+            elif letter==four:
+                fi_caract="X"
+            else:
+                fi_caract=" "
+        if c==1:
+            if letter==second:
+                se_caract=second
+            elif letter==first:
+                se_caract="X"
+            elif letter==third:
+                se_caract="X"
+            elif letter==four:
+                se_caract="X"
+            else:
+                se_caract=" "
+        if c==2:
+            if letter==third:
+                th_caract=third
+            elif letter==first:
+                th_caract="X"
+            elif letter==second:
+                th_caract="X"
+            elif letter==four:
+                th_caract="X"
+            else:
+                th_caract=" "
+        if c==3:
+            if letter==four:
+                fo_caract=four
+            elif letter==first:
+                fo_caract="X"
+            elif letter==second:
+                fo_caract="X"
+            elif letter==third:
+                fo_caract="X"
+            else:
+                fo_caract=" "
+        c+=1
+        
     else:
-        print("Essaye encore")
         a+=1
+        score=b-a
+    print(f"{fi_caract}{se_caract}{th_caract}{fo_caract}")
 
-    print(i)
+exit()
 
 
 
